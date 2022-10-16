27c63e3e9ed8928a4aeb2709249d33451ba19703
Yevhen <shershen.yevhen@gmai>
2022-10-16 17:35:57 +0200

    create component Persona-information.js

---
+++ src/components/personal-info/Personal-information.js
@@ -0,0 +1,21 @@
+import ListGroup from "react-bootstrap/ListGroup";
+import CloseButton from "../btns/Close-button";
+import Line from "../Line";
+const PersonalInformation = (props) => {
+  return (
+    <div className="personal-info table-border px-2">
+      <div className="panel">
+        <h2>Personal information</h2> <CloseButton />
+      </div>
+      <Line />
+      <ListGroup variant="flush">
+        <ListGroup.Item>Name: {props.personInfo.name}</ListGroup.Item>
+        <ListGroup.Item>Surname: {props.personInfo.surname}</ListGroup.Item>
+        <ListGroup.Item>
+          Personal ID: {props.personInfo.personId}
+        </ListGroup.Item>
+      </ListGroup>
+    </div>
+  );
+};
+export default PersonalInformation;

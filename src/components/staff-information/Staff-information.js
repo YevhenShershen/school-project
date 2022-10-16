9645fffb2089cde70c2461be5a86483c7df9f0ca
Yevhen <shershen.yevhen@gmai>
2022-10-16 19:58:33 +0200

    Create table in component StaffInformation

--- src/components/staff-information/Staff-information.js
+++ src/components/staff-information/Staff-information.js
@@ -1,40 +1,29 @@
 import Table from "react-bootstrap/Table";
 import CloseButton from "../btns/Close-button";
-const StaffInformation = () => {
+const StaffInformation = (props) => {
+  const columns = props.personalsInformation.map((el, index) => (
+    <tr  key={index}>
+      <td>{index + 1}</td>
+      <td>{el.name}</td>
+      <td>{el.surname}</td>
+      <td>{el.personId}</td>
+    </tr>
+  ));
   return (
     <div className="table-border ">
       <div className="panel">
         <h1>Staff Information</h1> <CloseButton />
       </div>
-
       <Table striped bordered hover>
         <thead>
           <tr>
             <th>#</th>
             <th>First Name</th>
             <th>Last Name</th>
-            <th>Username</th>
+            <th>Person ID</th>
           </tr>
         </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td colSpan={2}>Larry the Bird</td>
-            <td>@twitter</td>
-          </tr>
-        </tbody>
+        <tbody>{columns}</tbody>
       </Table>
     </div>
   );
